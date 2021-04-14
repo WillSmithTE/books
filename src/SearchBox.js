570db302312d66301acaa1e698a5fffa02d29c6c
Will Smith <william.smith@service.nsw.gov.au>
2021-04-14 19:43:14 +1000

    add textfield

---
+++ src/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {TextField} from '@material-ui/core'; 
+
+export const SearchBox = () => {
+    return   <TextField     style={{
+        backgroundColor: "white"
+    }}
+
+    id="outlined-basic" variant="outlined" />
+};
