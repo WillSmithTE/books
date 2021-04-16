822ec1e1ca638d1ab20d7131dfdb4c2bb8c39ea3
Will Smith <william.smith@service.nsw.gov.au>
2021-04-16 18:09:08 +1000

    add search button

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
+import { makeStyles } from '@material-ui/core/styles';
+import { SearchBoxContainer } from './SearchBox.styled';
 
-export const SearchBox = ({setSearchTerm}) => {
-    const { handleSubmit, register } = useForm();
-    const onSubmit = ({search}) => {
+const useStyles = makeStyles((theme) => ({
+    searchBar: {
+        backgroundColor: "white",
+        minWidth: '250px'
+    },
+    searchButton: {
+        marginTop: '1rem',
+        backgroundColor: '#FFE600',
+        '&:hover': {
+            backgroundColor: "#f00",
+         },
+    }
+}));
+
+export const SearchBox = ({ setSearchTerm }) => {
+    const { handleSubmit, register, watch } = useForm();
+    const { searchBar, searchButton } = useStyles();
+    const onSubmit = ({ search }) => {
         setSearchTerm(search);
     };
 
+    const showSearchButton = Boolean(watch('search'));
+    const getButtonStyle = () => showSearchButton ? {} : {visibility: 'hidden'};
+
     return <form onSubmit={handleSubmit(onSubmit)}>
-
-        <TextField
-            autoFocus
-            style={{
-                backgroundColor: "white",
-                minWidth: '250px'
-            }}
-            variant="outlined"
-            {...register('search')}
-        />
-        <input type="submit" hidden />
-
+        <SearchBoxContainer>
+            <TextField
+                autoFocus
+                className={searchBar}
+                variant="outlined"
+                {...register('search')}
+            />
+            <Button className={searchButton} variant='outlined' style={getButtonStyle()}>Get it free</Button>
+        </SearchBoxContainer>
     </form>
 };
