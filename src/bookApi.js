f41b88d4cf8f6fa744bc9df9237f529772e1ad5c
Will Smith <william.smith@service.nsw.gov.au>
2021-04-16 17:34:50 +1000

    add more books

--- src/bookApi.js
+++ src/bookApi.js
@@ -1,4 +1,9 @@
 import harrypotter from './resources/harrypotter.jpg';
+import howtomotivateyourchild from './resources/howtomotivateyourchild.jpeg';
+import michaeljordan from './resources/michaeljordan.jpeg';
+import thepowerofnow from './resources/thepowerofnow.jpeg';
+import lordoftherings from './resources/lordoftherings.jpeg';
+import range from './resources/range.jpeg';
 
 export const bookApi = {
     search: async (searchTerm) => {
@@ -8,18 +13,33 @@ export const bookApi = {
 
 const dummyBooks = [
     {
-        name: `Michael Jordan - The Life`,
+        name: `Harry Potter`,
         author: 'J.K. Rowling',
         imgUrl: harrypotter
+    },
+    {
+        name: `Michael Jordan - The Life`,
+        author: 'Roland',
+        imgUrl: michaeljordan
     },
     {
         name: `The Power of Now`,
-        author: 'J.K. Rowling',
-        imgUrl: harrypotter
+        author: 'Eckhart Tolle',
+        imgUrl: thepowerofnow
     },
     {
         name: `Lord of the Rings`,
-        author: 'J.K. Rowling',
-        imgUrl: harrypotter
+        author: 'J.R.R. Tolkien',
+        imgUrl: lordoftherings
+    },
+    {
+        name: `Range`,
+        author: 'David Epstein',
+        imgUrl: range
+    },
+    {
+        name: `How to Motivate Your Child`,
+        author: 'Andrew Martin',
+        imgUrl: howtomotivateyourchild
     },
 ]
